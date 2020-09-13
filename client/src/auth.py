641c4232965f885fbf822c453a188d7ccfdf66e1
kidush <thiago.evandson@gmail.com>
2020-09-13 16:30:46 -0300

    Add auth file on client side

---
+++ client/src/auth.py
@@ -0,0 +1,19 @@
+import json
+
+class Auth:
+    def __init__(self):
+        self._connection = None
+
+    def authenticate(self, connection):
+        self.__connection = connection
+
+        user = input('Informe seu usuário: ')
+        password = input('Inform sua senha: ')
+
+        self.__connection.send(json.dumps({'user': user, 'password': password}).encode())
+
+        response = self.__connection.recv(1024)
+        print(response.decode())
+
+
+
